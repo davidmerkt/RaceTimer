9e1e70a2c03d3789d5c79dce786367c934f8edd3
David Merkt <davidmerkt@users.noreply.github.com>
2018-01-11 22:59:14 -0500

    datamodel.js

---
+++ datamodel.js
@@ -0,0 +1,73 @@
+var timerEvent = [
+  {
+    "startTime": 1515646351322,
+    "endTime": 1515652511241,
+    "description": "",
+    "location": {
+      "latitude": "41.82284533029305",
+      "longitude": "-72.44302471066285"
+    }
+  },
+  {
+    "startTime": 1515723053075,
+    "endTime": null,
+    "description": "",
+    "location": {
+      "latitude": "41.82284533029305",
+      "longitude": "-72.44302471066285"
+    }
+  }
+];
+
+var timerPause_1515646351322 = [
+  {
+    "start": 1515647551322,
+    "end": 1515648051322
+  },
+  {
+    "start": 1515650551322,
+    "end": 1515651051322
+  }
+]
+
+var timerEventSplit_1515646351322 = [
+  {
+    "splitTime": 1515647351322,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515648351322,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515649351322,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515650351322,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515651351322,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515652351322,
+    "deleted": false
+  }
+];
+
+var timerEventSplit_1515723053075 = [
+  {
+    "splitTime": 1515724053075,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515725053075,
+    "deleted": false
+  },
+  {
+    "splitTime": 1515726053075,
+    "deleted": false
+  }
+];
